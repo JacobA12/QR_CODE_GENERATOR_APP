bfba9651af815349c428e2755f32537c0b347893
Jacob Adams big-boi <jacob.adams121202@gmail.com>
2024-08-15 19:57:38 -0700

    added spinner functionality

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
-const form = document.getElementById('generate-form');
-const qr = document.getElementById('qrcode');
+const form = document.getElementById("generate-form");
+const qr = document.getElementById("qrcode");
 
 const onGenerateSubmit = (e) => {
   e.preventDefault();
 
-  const url = document.getElementById('url').value;
-  const size = document.getElementById('size').value;
+  const url = document.getElementById("url").value;
+  const size = document.getElementById("size").value;
 
-  console.log(url,size);
-}
+  if (url === "") {
+    alert("Please enter a URL");
+  } else {
+    showSpinner();
 
-form.addEventListener('submit', onGenerateSubmit);
+    setTimeout(() => {
+      hideSpinner();
+    }, 1000);
+  }
+};
+
+const showSpinner = () => {
+  document.getElementById("spinner").style.display = "block";
+};
+const hideSpinner = () => {
+  document.getElementById("spinner").style.display = "none";
+};
+
+hideSpinner();
+
+form.addEventListener("submit", onGenerateSubmit);
